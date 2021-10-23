2816ce0701da5ff354eae09b1c2aa1bddddb7713
Swayantan <swayantanmaji815@gmail.com>
2021-10-23 09:31:38 +0530

    initial commit

---
+++ sun.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    system("cls");
+    int s,r;
+    cout<<"Enter 2 no.s:";
+    cin>>s>>r;
+    cout<<"sum of 2 no.s is:"<<s+r;
+    return 0;
+}
